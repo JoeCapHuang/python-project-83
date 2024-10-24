edb7e99ca3c09c7922866cb8750bd48d4feaa676
JoeCapHuang <kapt.tesla@gmail.com>
2024-10-24 17:33:52 +0300

    update app

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
-import validators
-import os
 from dotenv import load_dotenv
-from .url_repositories import URLRepository
-
+from .tools import validate_url
+from .service.url_service import (
+    create_url,
+    list_urls,
+    get_url_details,
+    perform_url_check
+)
+import os
 
 load_dotenv()
 app = Flask(__name__)
@@ -17,51 +21,31 @@ def index():
 
 @app.route('/urls', methods=['GET'])
 def urls_index():
-    repo = URLRepository()
-    all_urls = repo.get_all_urls()
-    repo.close()
-    return render_template('urls/index.html', urls=all_urls)
+    urls = list_urls()
+    return render_template('urls/index.html', urls=urls)
 
 
 @app.route('/urls', methods=['POST'])
 def urls_post():
     url = request.form.get('url')
-
-    if len(url) > 255:
-        flash('URL превышает 255 символов', 'danger')
-        return redirect(url_for('index'))
-
-    if not url:
-        flash('URL не может быть пустым', 'danger')
-        return redirect(url_for('index'))
-
-    if validators.url(url):
-        repo = URLRepository()
-        repo_id, message = repo.add_url(url)
-        repo.close()
+    validity, message = validate_url(url)
+    if not validity:
         flash(*message)
-        return redirect(url_for('url_show', url_id=repo_id))
-
-    else:
-        flash('Некорректный URL', 'danger')
         return redirect(url_for('index'))
+
+    url_id, message = create_url(url)
+    flash(*message)
+    return redirect(url_for('url_show', url_id=url_id))
 
 
 @app.route('/urls/<url_id>')
 def url_show(url_id):
-    repo = URLRepository()
-    url_data = repo.get_url_by_id(url_id)
-    checks = repo.get_all_checks(url_id)
-    repo.close()
-
+    url_data, checks = get_url_details(url_id)
     return render_template('urls/show.html', url=url_data, checks=checks)
 
 
 @app.route('/urls/<url_id>/checks', methods=['POST'])
 def checks_post(url_id):
-    repo = URLRepository()
-    check = {}
-    repo.add_check(url_id, check)
-    repo.close()
-    flash('Страница успешно проверена', 'success')
-    return redirect((url_for('url_show', url_id=url_id)))
+    message = perform_url_check(url_id)
+    flash(*message)
+    return redirect(url_for('url_show', url_id=url_id))
