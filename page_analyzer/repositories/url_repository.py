5eda55df6f75581fd27c4c8ffb7e919651143b4f
JoeCapHuang <kapt.tesla@gmail.com>
2024-10-24 17:32:05 +0300

    split repo to url and url_check

--- page_analyzer/repositories/url_repository.py
+++ page_analyzer/repositories/url_repository.py
@@ -1,96 +1,50 @@
-import psycopg2
-from psycopg2.extras import DictCursor
-import os
-from dotenv import load_dotenv
-from urllib.parse import urlparse, urlunparse
 from datetime import datetime
+from page_analyzer.tools import get_name_from_url
 
 
-load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL')
+def add_url(conn, url):
+    with conn.cursor() as cur:
+        name = get_name_from_url(url)
+        url_id = get_url_by_name(conn, name)
+
+        if url_id:
+            return url_id['id'], ('Страница уже существует', 'info')
+
+        cur.execute("INSERT INTO urls (name, created_at)"
+                    "VALUES (%s, %s) RETURNING id;", (name, datetime.now()))
+        conn.commit()
+
+        url_id = cur.fetchone()
+        return url_id['id'], ('Страница успешно добавлена', 'success')
 
 
-class URLRepository:
+def get_all_urls(conn):
+    with conn.cursor() as cur:
+        cur.execute("""
+            SELECT
+                urls.id,
+                urls.name,
+                url_checks.status_code AS last_status_code,
+                url_checks.created_at AS last_check_date
+            FROM
+                urls
+            LEFT JOIN (
+                SELECT DISTINCT ON (url_id) *
+                FROM url_checks
+                ORDER BY url_id, created_at DESC
+            ) AS url_checks ON urls.id = url_checks.url_id
+            ORDER BY urls.id;
+        """)
+        return cur.fetchall()
 
-    def __init__(self):
-        self.conn = psycopg2.connect(DATABASE_URL, cursor_factory=DictCursor)
 
-    def add_url(self, url):
-        with self.conn.cursor() as cur:
-            parsed_url = urlparse(url)
-            name = urlunparse((parsed_url.scheme, parsed_url.netloc, '', '', '', ''))
+def get_url_by_name(conn, name):
+    with conn.cursor() as cur:
+        cur.execute("SELECT id FROM urls WHERE name = %s;", (name,))
+        return cur.fetchone()
 
-            cur.execute("SELECT id FROM urls WHERE name = %s;", (name,))
-            url_id = cur.fetchone()
 
-            if url_id:
-                return url_id['id'], ('Страница уже существует', 'info')
-
-            date = datetime.now()
-            cur.execute("INSERT INTO urls (name, created_at)"
-                        "VALUES (%s, %s) RETURNING id;", (name, date))
-            self.conn.commit()
-
-            url_id = cur.fetchone()
-            return url_id['id'], ('Страница успешно добавлена', 'success')
-
-    def get_all_urls(self):
-        with self.conn.cursor() as cur:
-            cur.execute("""
-                SELECT
-                    urls.id,
-                    urls.name,
-                    url_checks.status_code AS last_status_code,
-                    url_checks.created_at AS last_check_date
-                FROM
-                    urls
-                LEFT JOIN (
-                    SELECT DISTINCT ON (url_id) *
-                    FROM url_checks
-                    ORDER BY url_id, created_at DESC
-                ) AS url_checks ON urls.id = url_checks.url_id
-                ORDER BY urls.id;
-            """)
-            return cur.fetchall()
-
-    def get_url_by_id(self, url_id):
-        with self.conn.cursor() as cur:
-            cur.execute("SELECT * FROM urls WHERE id = %s;", (url_id,))
-            return cur.fetchone()
-
-    def add_check(self, url_id, check: dict):
-        status_code = check.get('status_code')
-        h1 = check.get('h1')
-        title = check.get('title')
-        description = check.get('description')
-        with self.conn.cursor() as cur:
-            cur.execute(
-                """
-                INSERT INTO url_checks
-                (url_id, status_code, h1, title, description, created_at)
-                VALUES (%s, %s, %s, %s, %s, %s)
-                RETURNING id;
-                """,
-                (
-                    url_id,
-                    status_code,
-                    h1,
-                    title,
-                    description,
-                    datetime.now()
-                )
-            )
-            self.conn.commit()
-            check_id = cur.fetchone()
-            return check_id
-
-    def get_all_checks(self, url_id):
-        with self.conn.cursor() as cur:
-            cur.execute("SELECT * FROM url_checks "
-                        "WHERE url_id = %s "
-                        "ORDER BY created_at DESC;", (url_id,))
-            return cur.fetchall() or []
-
-    def close(self):
-        if self.conn:
-            self.conn.close()
+def get_url_by_id(conn, url_id):
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM urls WHERE id = %s;", (url_id,))
+        return cur.fetchone()
